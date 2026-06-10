d78964c99a7ecf073447c373f9b041c334272805
Alex <atheinrich@outlook.com>
2026-06-10 16:43:41 -0700

    test 3

--- _push_output.bat
+++ _push_output.bat
@@ -6,6 +6,10 @@ if exist output rmdir /s /q output
 pelican content
 xcopy extra output /E /I /Y >nul
 
+:: Add workflow so GitHub can deploy output branch to Nekoweb
+mkdir output\.github\workflows
+copy .github\workflows\deploy-nekoweb.yml output\.github\workflows\deploy-nekoweb.yml >nul
+
 :: Deploy the generated site to output branch
 ghp-import output -b output
 git fetch origin output
