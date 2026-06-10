50a52c7eec2903dda943a3b60b38c3c1662161bf
Alex <atheinrich@outlook.com>
2026-06-10 16:54:23 -0700

    test 6

--- _push_output.bat
+++ _push_output.bat
@@ -8,7 +8,7 @@ xcopy extra output /E /I /Y >nul
 
 :: Add GitHub Actions workflow to output branch
 mkdir output\.github\workflows
-copy .github\workflows\deploy-nekoweb.yml output\.github\workflows\deploy-nekoweb.yaml >nul
+copy .github\workflows\deploy-nekoweb.yaml output\.github\workflows\deploy-nekoweb.yaml >nul
 
 :: Deploy the generated site to output branch
 ghp-import output -b output
